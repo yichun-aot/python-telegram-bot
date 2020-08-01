e17566f0b94d5ea077fa044fd81689db2a57ce65
chunthebear <zyc5026@hotmail.com>
2020-08-01 15:26:10 -0700

    Added documentation

--- app.py
+++ app.py
@@ -3,17 +3,27 @@ from flask import Flask, request, jsonify
 from telegram_bot import TelegramBot
 from config import TELEGRAM_INIT_WEBHOOK_URL
 
+# Create app object which is our app server
 app = Flask(__name__)
+
+# Provides the telegram server with our Ngrok URL
 TelegramBot.init_webhook(TELEGRAM_INIT_WEBHOOK_URL)
 
+# Actual local endpoint
 @app.route('/webhook', methods=['POST'])
 def index():
+    # Get Json data
     req = request.get_json()
+    # Create bot object
     bot = TelegramBot()
+    # Feed Json data to the bot
     bot.parse_webhook_data(req)
+    # Get action from bot depending on the Json data
     success = bot.action()
+    # Respond the boolean value of success to Telegram server to not repeat sending the data
     return jsonify(success=success) # TODO: Success should reflect the success of the reply
 
+# Only execute if the current py script is executed by itself, not other modules
 if __name__ == '__main__':
     app.run(port=5000)
 
--- telegram_bot.py
+++ telegram_bot.py
@@ -4,47 +4,41 @@ from config import TELEGRAM_SEND_MESSAGE_URL
 
 class TelegramBot:
 
+    """
+    Initializes an instance of the TelegramBot class.
+
+    Attributes:
+        chat_id:str: Chat ID of Telegram chat, used to identify which conversation outgoing messages should be send to.
+        text:str: Text of Telegram chat
+        first_name:str: First name of the user who sent the message
+        last_name:str: Last name of the user who sent the message
+    """
     def __init__(self):
-        """"
-        Initializes an instance of the TelegramBot class.
-
-        Attributes:
-            chat_id:str: Chat ID of Telegram chat, used to identify which conversation outgoing messages should be send to.
-            text:str: Text of Telegram chat
-            first_name:str: First name of the user who sent the message
-            last_name:str: Last name of the user who sent the message
-        """
-
         self.chat_id = None
         self.text = None
         self.first_name = None
         self.last_name = None
 
+    """
+    Parses Telegram JSON request from webhook and sets fields for conditional actions
 
+    Args:
+        data:str: JSON string of data
+    """
     def parse_webhook_data(self, data):
-        """
-        Parses Telegram JSON request from webhook and sets fields for conditional actions
-
-        Args:
-            data:str: JSON string of data
-        """
-
         message = data['message']
-
         self.chat_id = message['chat']['id']
         self.incoming_message_text = message['text'].lower()
         self.first_name = message['from']['first_name']
         self.last_name = message['from']['last_name']
 
+    """
+    Conditional actions based on set webhook data.
 
+    Returns:
+        bool: True if the action was completed successfully else false
+    """
     def action(self):
-        """
-        Conditional actions based on set webhook data.
-
-        Returns:
-            bool: True if the action was completed successfully else false
-        """
-
         success = None
 
         if self.incoming_message_text == '/hello':
@@ -57,26 +51,21 @@ class TelegramBot:
         
         return success
 
-
+    """
+    Sends message to Telegram servers.
+    """
     def send_message(self):
-        """
-        Sends message to Telegram servers.
-        """
-
         res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(self.chat_id, self.outgoing_message_text))
-
         return True if res.status_code == 200 else False
     
+    """
+    Initializes the webhook
 
+    Args:
+        url:str: Provides the telegram server with a endpoint for webhook data
+    """
     @staticmethod
     def init_webhook(url):
-        """
-        Initializes the webhook
-
-        Args:
-            url:str: Provides the telegram server with a endpoint for webhook data
-        """
-
         requests.get(url)
 
 
